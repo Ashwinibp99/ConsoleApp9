2b6063e739cce2275dd3a8dac66445d02e17722f
vijay <vijay@DESKTOP-Q54HVU8>
2021-10-05 04:08:38 -0700

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class FibonancciSeries
+    {
+        int[] arr = new int[] { 20, 30, 10, 5, 20, 30, 15, 10, 2 };
+        int count = 0;
+        
+
+        public FibonancciSeries()
+        {
+            int l = arr.Length;
+
+            for (int i=0;i<=l;i++)
+            {
+                for(int j=1;j<=l;j++)
+                {
+                    if(arr[i]==arr[j])
+                    {
+                        count++;
+                        Console.WriteLine(arr[i] + "repeated" + count);
+                    }
+                }
+            }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonancciSeries fib = new FibonancciSeries();
+        }
+    }
+}
